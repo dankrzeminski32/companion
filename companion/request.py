3e38677c76c45909c9b8376272bf083ed79d1873
dankrzeminski32 <dankrzeminski32@gmail.com>
2025-02-08 19:25:56 -0600

    use enum in request class

--- companion/request.py
+++ companion/request.py
@@ -1,18 +1,22 @@
+from companion.enums import HttpMethod
+
+
+class HttpRequestLine(object):
+    def __init__(self, method: HttpMethod, target: str, version: str):
+        self.method = method
+        self.target = target
+        self.version = version
+
+    def __str__(self):
+        return f"HttpRequestLine(method={self.method}, uri={self.uri}, version={self.version})"
+
 
 class HttpRequest(object):
-    def __init__(self, request_line, headers=None, body=None):
+    def __init__(self, request_line: HttpRequestLine, headers=None, body=None):
         self.request_line = request_line
+        self.method = self.request_line.method
         self.headers = headers
         self.body = body
 
     def __str__(self):
         return f"HttpRequest(request_line={self.request_line}, headers={self.headers}, body={self.body})"
-
-class HttpRequestLine(object):
-    def __init__(self, method, uri, version):
-        self.method = method
-        self.uri = uri
-        self.version = version
-
-    def __str__(self):
-        return f"HttpRequestLine(method={self.method}, uri={self.uri}, version={self.version})"
