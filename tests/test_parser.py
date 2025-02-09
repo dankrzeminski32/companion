30df29c535c86928559f47eb4d663a7a7e79f3eb
dankrzeminski32 <dankrzeminski32@gmail.com>
2025-02-08 19:26:22 -0600

    additional parser tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,12 +1,17 @@
 from companion.parser import HttpParser
+from companion.enums import HttpMethod
+from companion.request import HttpRequest, HttpRequestLine
 
-GET_REQ_WITH_HEADERS = b"Get /mysite/index.html HTTP/1.1\r\nHost: 10.101.101.10\r\nAccept: */*\r\n\r\n"
+GET_REQ_WITH_HEADERS = b"GET /mysite/index.html HTTP/1.1\r\nHost: 10.101.101.10\r\nAccept: */*\r\n\r\n"
 
 def test_parse_get_request():
     parser = HttpParser(GET_REQ_WITH_HEADERS)
     result = parser.parse()
-    assert result.request_line.method == "Get"
-    assert result.request_line.uri == "/mysite/index.html"
+    assert isinstance(result, HttpRequest) is True
+    assert isinstance(result.request_line, HttpRequestLine) is True
+    assert result.request_line.method == HttpMethod.GET
+    assert result.request_line.target == "/mysite/index.html"
     assert result.request_line.version == "HTTP/1.1"
     assert result.headers == {"Host": "10.101.101.10", "Accept": "*/*"}
-    assert result.body == None
+    assert result.body is None
+
