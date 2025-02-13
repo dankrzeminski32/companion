73bfe930adc5fad5ebb1efb6e66db693c338b71f
dankrzeminski32 <dankrzeminski32@gmail.com>
2025-02-12 22:35:24 -0600

    fix: add Server and Content-Length response headers

--- companion/handler.py
+++ companion/handler.py
@@ -16,6 +16,7 @@ class HttpRequestHandler(object):
     def get(self, request_target: str, headers: dict):
         clean_request_target = request_target.lstrip("/")
         target_file_or_directory = (self.file_directory / clean_request_target).resolve()
+        response_headers = {"Server": "companion"}
 
         if self.file_directory in target_file_or_directory.parents or target_file_or_directory == self.file_directory:
             if target_file_or_directory.exists():
@@ -23,12 +24,14 @@ class HttpRequestHandler(object):
                     if (target_file_or_directory / "index.html").exists():
                         with (target_file_or_directory / "index.html").open("rb") as fp:
                             content = fp.read()
-                            return HttpResponse(HttpStatus.OK, body=content)
+                            response_headers["Content-Length"] = len(content)
+                            return HttpResponse(HttpStatus.OK, body=content, headers=response_headers)
                 else:
                     with target_file_or_directory.open("rb") as fp:
                         content = fp.read()
-                        return HttpResponse(HttpStatus.OK, body=content)
-        return HttpResponse(HttpStatus.NOT_FOUND)
+                        response_headers["Content-Length"] = len(content)
+                        return HttpResponse(HttpStatus.OK, body=content, headers=response_headers)
+        return HttpResponse(HttpStatus.NOT_FOUND, headers=response_headers)
 
     def head(self, request_target: str, headers: dict):
         ...
--- tests/test_request_handler.py
+++ tests/test_request_handler.py
@@ -6,7 +6,7 @@ def test_GET_request_for_nonexistant_file_returns_404(tmp_path):
     assert resp.status_code == 404
     assert resp.status_code_message == "Not Found"
     assert resp.body is None
-    assert resp.bytes == b"HTTP/1.0 404 Not Found\r\n\r\n"
+    assert resp.bytes == b"HTTP/1.0 404 Not Found\r\nServer: companion\r\n\r\n"
 
 def test_GET_request_for_existing_html_file_returns_200_resp(tmp_path):
     handler = HttpRequestHandler(tmp_path)
@@ -15,7 +15,7 @@ def test_GET_request_for_existing_html_file_returns_200_resp(tmp_path):
     assert resp.status_code == 200
     assert resp.status_code_message == "OK"
     assert resp.body == b"<div>test</div>"
-    assert resp.bytes == b"HTTP/1.0 200 OK\r\n\r\n<div>test</div>"
+    assert resp.bytes == b"HTTP/1.0 200 OK\r\nServer: companion\r\nContent-Length: 15\r\n\r\n<div>test</div>"
 
 def test_get_request_for_path_with_default_indexhtml_returns_200_resp(tmp_path):
     handler = HttpRequestHandler(tmp_path)
@@ -24,7 +24,7 @@ def test_get_request_for_path_with_default_indexhtml_returns_200_resp(tmp_path):
     assert resp.status_code == 200
     assert resp.status_code_message == "OK"
     assert resp.body == b"<div>test</div>"
-    assert resp.bytes == b"HTTP/1.0 200 OK\r\n\r\n<div>test</div>"
+    assert resp.bytes == b"HTTP/1.0 200 OK\r\nServer: companion\r\nContent-Length: 15\r\n\r\n<div>test</div>"
 
 def test_get_request_with_target_outside_folder_returns_404(tmp_path):
     handler = HttpRequestHandler(tmp_path)
@@ -33,4 +33,4 @@ def test_get_request_with_target_outside_folder_returns_404(tmp_path):
     assert resp.status_code == 404
     assert resp.status_code_message == "Not Found"
     assert resp.body is None
-    assert resp.bytes == b"HTTP/1.0 404 Not Found\r\n\r\n"
+    assert resp.bytes == b"HTTP/1.0 404 Not Found\r\nServer: companion\r\n\r\n"
