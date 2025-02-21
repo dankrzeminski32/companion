895d81fe0425bb6c849ea74c6561f644de4fcfcf
dankrzeminski32 <dankrzeminski32@gmail.com>
2025-02-20 21:13:08 -0600

    refactor most of the server module into HttpServer class

--- companion/server.py
+++ companion/server.py
@@ -21,92 +21,85 @@ PORT = 8181
 CHUNK_SIZE = 1024
 END_HTTP_REQUEST = "\r\n\r\n"
 
-STATIC_FILE_DIR = Path("C:/Users/dankr/OneDrive/Desktop/companion/testfolder")
 
-inputs = []
-outputs = []
-exceptions = []
+class HttpServer:
+    inputs = []
+    outputs = []
+    exceptions = []
+    messages = defaultdict(queue.Queue)
 
-messages = defaultdict(queue.Queue)
-
-
-def read_http_request(connection):
-    found_end = False
-    data = b""
-    while not found_end:
-        data += connection.recv(CHUNK_SIZE)
-        if not data:
-            return None
-        if data.decode("ascii")[-4:] == END_HTTP_REQUEST:
-            found_end = True
-    return data
-
-
-def handle_read(connection, request_handler):
-    http_request_bytes = read_http_request(connection)
-    if http_request_bytes:
-        http_request = HttpParser(http_request_bytes).parse()
-        logger.info(f"Incoming Request {http_request}")
-        http_response = request_handler.handle(http_request)
-        messages[connection].put_nowait(http_response.bytes)
-        return True
-    else:
-        connection.close()
-        return False
-
-
-
-def handle_write(connection: socket.socket):
-    message = messages[connection].get_nowait()
-    logger.info(f"Sending Response {message}")
-    connection.sendall(message)
-    connection.close()
+    def __init__(self, staticdir: Path, port: int, host: str):
+        logger.info(f"Setting static content directory {staticdir}")
+        self.staticdir = staticdir
+        self.request_handler = HttpRequestHandler(file_directory=self.staticdir)
+        self.port = port
+        self.host = host
+        logger.info("Creating Server socket")
+        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_sock.setblocking(0)
+        self.server_sock.bind((host, port))
+        self.server_sock.listen(5)
     
+    def run(self):
+        self.inputs.append(self.server_sock)
+        try:
+            while True:
+                read_list, write_list, exception_list = select.select(self.inputs, self.outputs, self.exceptions)
+                for conn in read_list:
+                    if conn == self.server_sock:
+                        new_connection, address = conn.accept()
+                        new_connection.setblocking(0)
+                        self.inputs.append(new_connection)
+                        break
+                    is_ready_for_send = self.handle_read(conn)
+                    self.inputs.remove(conn)
+                    if is_ready_for_send:
+                        self.outputs.append(conn)
+                for conn in write_list:
+                    self.handle_write(conn)
+                    self.outputs.remove(conn)
+                    conn.close()
+                for conn in exception_list:
+                    self.handle_exception(conn)
+        except Exception as exc:
+            logger.exception(exc)
+            self.server_sock.close()
 
-def handle_exception(connection):
-    logger.info("Handling exception connection")
-    ...
+    def handle_read(self, connection):
+        http_request_bytes = self.read_http_request(connection)
+        if http_request_bytes:
+            http_request = HttpParser(http_request_bytes).parse()
+            logger.info(f"Incoming Request {http_request}")
+            http_response = self.request_handler.handle(http_request)
+            self.messages[connection].put_nowait(http_response.bytes)
+            return True
+        else:
+            connection.close()
+            return False
 
+    def handle_write(self, connection: socket.socket):
+        message = self.messages[connection].get_nowait()
+        logger.info(f"Sending Response {message}")
+        connection.sendall(message)
+        connection.close()
 
-def initialize_server_socket() -> socket.socket:
-    logger.info("Creating Server socket")
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setblocking(0)
-    sock.bind((HOST, PORT))
-    sock.listen(5)
-    return sock
-
-
-def run_forever(staticdir: Path):
-    logger.info(f"Setting static content directory {staticdir.resolve()}")
-    request_handler = HttpRequestHandler(file_directory=staticdir)
-    server_socket = initialize_server_socket()
-    inputs.append(server_socket)
-    try:
-        while True:
-            read_list, write_list, exception_list = select.select(inputs, outputs, exceptions)
-            for conn in read_list:
-                if conn == server_socket:
-                    new_connection, address = conn.accept()
-                    new_connection.setblocking(0)
-                    inputs.append(new_connection)
-                    break
-                is_ready_for_send = handle_read(conn, request_handler)
-                inputs.remove(conn)
-                if is_ready_for_send:
-                    outputs.append(conn)
-            for conn in write_list:
-                handle_write(conn)
-                outputs.remove(conn)
-                conn.close()
-            for conn in exception_list:
-                handle_exception(conn)
-    except Exception as exc:
-        logger.exception(exc)
-        server_socket.close()
+    def handle_exception(self, connection):
+        logger.info("Handling exception connection")
+        ...
+        
+    def read_http_request(self, connection):
+        found_end = False
+        data = b""
+        while not found_end:
+            data += connection.recv(CHUNK_SIZE)
+            if not data:
+                return None
+            if data.decode("ascii")[-4:] == END_HTTP_REQUEST:
+                found_end = True
+        return data
 
 
 def cli():
     args = argparser.parse_args()
     static_content_dir = Path(args.staticdir).resolve()
-    run_forever(static_content_dir)
+    HttpServer(static_content_dir, PORT, HOST).run()
